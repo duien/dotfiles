63c7e12a709d45cc0854580a721f05d86aa999f8
Emily Hyland <ehyland@newrelic.com>
2013-12-30 09:54:05 -0800

    Move Ruby info into prompt, no more rprompt

--- home/.config/fish/functions/fish_right_prompt.fish
+++ home/.config/fish/functions/fish_right_prompt.fish
@@ -1,8 +1,6 @@
 function fish_right_prompt --description 'Write out the right prompt'
 
-  set_color $fish_color_virtualenv
-
-  # ruby -v | awk '{split($0,a," "); print a[2]}'
-  ruby -v | cut -f 2 -d ' '
+  # set_color $fish_color_virtualenv
+  # prompt_ruby
 
 end
