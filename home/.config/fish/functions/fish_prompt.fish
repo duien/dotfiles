1d57ca935fae87e6cf5becb65cd05b9349fffc9f
Emily Hyland <price.emily@gmail.com>
2014-02-27 23:13:12 -0800

    Use basic colors for prompt segments

--- home/.config/fish/functions/fish_prompt.fish
+++ home/.config/fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt --description 'Write out the prompt'
-	
+
 	set -l last_status $status
 
 	if not set -q __fish_prompt_normal
@@ -7,11 +7,14 @@ function fish_prompt --description 'Write out the prompt'
 	end
 
   set -xg previous_background "start"
-	fish_prompt_segment white 005F87 (prompt_pwd)
-  fish_prompt_segment d70000 5f0000 ⟡ (prompt_ruby)
+	# fish_prompt_segment white 005F87 (prompt_pwd)
+	fish_prompt_segment white blue (prompt_pwd)
+  # fish_prompt_segment d70000 5f0000 ⟡ (prompt_ruby)
+	fish_prompt_segment black red ⟡ (prompt_ruby)
   fish_git_prompt
-  if not test $last_status -eq 0 
-    fish_prompt_segment d70000 black "➥ $last_status"
+  if not test $last_status -eq 0
+    # fish_prompt_segment d70000 black "➥ $last_status"
+		fish_prompt_segment red black "➥ $last_status"
   end
   fish_prompt_end
 
--- home/.config/fish/functions/prompt_pwd.fish
+++ home/.config/fish/functions/prompt_pwd.fish
@@ -2,15 +2,21 @@ function prompt_pwd --description "Print the current working directory, shortene
   set -l directory_parts (echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||' | tr / \n)
   # echo $directory_parts[-3 -2 -1]
 
-  set_color --background 005F87 white
+  # set -l background_color 005F87
+  set -l background_color blue
+  set -l segment_color white
+  # set -l separator_color blue
+  set -l separator_color cyan
+
+  set_color --background $background_color $segment_color
 
   set -l start -4
 
   if set -q directory_parts[-5]
     echo -n '⋯ '
-    set_color blue
+    set_color $separator_color
     echo -n '  '
-    set_color white
+    set_color $segment_color
     set start -3
   end
 
@@ -18,14 +24,14 @@ function prompt_pwd --description "Print the current working directory, shortene
     if set -q directory_parts[$i]
       set -l part $directory_parts[$i]
       echo -n "$part"
-      set_color blue
+      set_color $separator_color
       echo -n '  '
-      set_color white
+      set_color $segment_color
     end
   end
 
-  set_color --bold white
+  set_color --bold $segment_color
   echo -n $directory_parts[-1]
   set_color normal
-  set_color --background 005F87
+  set_color --background $background_color
 end
