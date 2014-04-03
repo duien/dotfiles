1025133891d58f55da36af33052f5acb03a6a3b6
Emily Hyland <ehyland@newrelic.com>
2014-04-03 12:00:42 -0700

    Recompile all the rubyies

---
+++ home/.config/fish/functions/recompile_all_the_rubies.fish
@@ -0,0 +1,35 @@
+function recompile_all_the_rubies -d "Uninstall all the rbenv rubies and recompile them"
+  if test (count $argv) -eq 1
+    echo "Usage:"
+    echo "    recompile_all_the_rubies [--because library]"
+    return
+  end
+  
+  set_color green
+  echo "Update homebrew"
+  set_color normal
+  brew update
+  
+  if test (count $argv) -gt 1
+    if test '--because' = $argv[1]
+      set_color green
+      echo "Upgrade $argv[2]"
+      set_color normal
+      brew upgrade $argv[2]
+    end
+  end
+  
+  set_color green
+  echo "Upgrade ruby-build"
+  set_color normal
+  brew upgrade ruby-build
+  
+  
+  for ruby in (rbenv versions --bare)
+    set_color red
+    echo "Uninstall and re-install $ruby"
+    set_color normal
+    rbenv uninstall $ruby
+    rbenv install $ruby
+  end
+end
