47dabf346a0f6b5c426c203d61269ed8e5f37667
Emily Hyland <price.emily@gmail.com>
2014-10-30 19:15:13 -0700

    Much better ruby prompt

--- home/.config/fish/functions/prompt_ruby.fish
+++ home/.config/fish/functions/prompt_ruby.fish
@@ -1,18 +1,23 @@
 function prompt_ruby
+  # Maybe use ∗ for global and ⟡ for local
   if which rbenv | grep 'rbenv' > /dev/null
-    for env in local global
-      set -l ruby_version (rbenv $env ^ /dev/null)
-      if [ -n "$ruby_version" ]
-        echo -n $ruby_version \($env\)
-        break
-      end
-      # if rbenv $env | grep 'version configured' > /dev/null ^ /dev/null
-      #   # no version configured
-      #   echo -n "no $env"
-      # else
-      #   echo -n "yes $env"
-      # end
+    # Check for local ruby version
+    set -l ruby_version (rbenv local ^ /dev/null)
+    if [ -n "$ruby_version" ]
+      echo -n "⟡ $ruby_version"
+      return
     end
+
+    # Check for global ruby version
+    set -l ruby_version (rbenv global ^ /dev/null)
+    if [ -n "$ruby_version" ]
+      echo -n "∗ $ruby_version"
+      return
+    end
+
+    # Huh, that's weird
+    set -l ruby_version (rbenv version ^ /dev/null)
+    echo -n \? ($ruby_version | cut -f 1 -d ' ')
   else
     echo -n (ruby -v | cut -f 2 -d ' ' | cut -f 1 -d 'p') # ruby version
   end
