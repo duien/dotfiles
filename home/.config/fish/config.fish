2c21d864b781f1369c791dbe855c1ba4576eb5b7
Emily Hyland <price.emily@gmail.com>
2015-07-24 01:21:30 -0700

    Trying out oh-my-fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,52 +1,20 @@
-alias gst "git status"
-alias d todo2
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
 
-alias ... ../..
-alias .... ../../..
-alias ..... ../../../..
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
 
-# echo $PWD | sed -e "s|^$HOME|~|" -e 's|^/private||'
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
 
-# __fish_git_prompt_showstashstate
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'cmorrell.com'
+Plugin 'theme'
+Plugin 'msg'
 
-. $HOME/.config/fish/solarized.fish
+# Help the theme work
+set -x default_user duien
 
-set CDPATH . $HOME $HOME/Code $CDPATH
-set PATH /usr/local/bin $PATH
-if test -d "$HOME/bin"
-  set PATH $HOME/bin $PATH
-end
-
-# apparently necessary for larger node builds, since it tries
-# to open all files at once
-ulimit -n 4096
-
-set -x EDITOR vim
-set -x VISUAL atom
-set -x GEMEDITOR atom
-
-# set -x DOCKER_HOST 127.0.0.1:32000
-if type boot2docker >/dev/null 2>&1
-  # set -x DOCKER_HOST tcp://(boot2docker ip 2>/dev/null):2376
-  # set -x DOCKER_CERT_PATH /Users/ehyland/.boot2docker/certs/boot2docker-vm
-  # set -x DOCKER_TLS_VERIFY 1
-
-  # Do boot2docker setup by using its `shellinit` function, which adapts to the
-  # actual env that it's setting up
-  eval (boot2docker shellinit | tr \n \;)
-end
-
-set -x RUBY_CONFIGURE_OPTS --with-readline-dir=(brew --prefix readline)
-set fish_color_virtualenv red
-
-# If `.private.sh` exsits, load it
-# This file is for ENV variables that shouldn't be checked in, such as tokens and API keys
-if test -s "$HOME/.private.sh" ; source "$HOME/.private.sh" ; end
-
-# Enable RBENV
-# set PATH $HOME/.rbenv/bin $PATH
-# set PATH $HOME/.rbenv/shims $PATH
-# rbenv rehash >/dev/null ^&1
-
-set -gx RBENV_ROOT /usr/local/var/rbenv
-. (rbenv init -|psub)
+# Load my aliases and environment
+source ~/.config/fish/more.fish
