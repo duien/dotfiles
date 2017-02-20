e840dad31c7fc37686cf329f36aa2270fb23e089
Emily Hyland <hello@duien.com>
2017-02-20 14:12:08 -0800

    Get rid of now-unnecessary fish initialization

--- home/.config/fish/more.fish
+++ home/.config/fish/more.fish
@@ -10,7 +10,7 @@ alias more-fish "source ~/.config/fish/more.fish"
 
 # Basic environment
 set CDPATH . $HOME $HOME/Code $CDPATH
-set PATH /usr/local/bin $PATH
+set PATH /usr/local/bin /usr/local/sbin $PATH
 if test -d "$HOME/bin"
   set PATH $HOME/bin $PATH
 end
@@ -19,28 +19,16 @@ if test -d "$HOME/.bin"
   set PATH $HOME/.bin $PATH
 end
 
-set -x EDITOR nvim
-set -x VISUAL nvim
-set -x GEMEDITOR nvim
+set -x EDITOR vim
+set -x VISUAL atom
+set -x GEMEDITOR atom
 
 # Source color theme setup
 source ~/.config/fish/color-theme.fish
 
 # Setting up other random crap
 
-# apparently necessary for larger node builds, since it tries
-# to open all files at once
-ulimit -n 4096
-
-if type boot2docker >/dev/null 2>&1
-  set -x DOCKER_HOST tcp://192.168.59.103:2375
-  # eval (boot2docker shellinit | tr \n \;)
-end
-
-if test -s  ~/.config/fish/nvm-wrapper/nvm.fish
-  source ~/.config/fish/nvm-wrapper/nvm.fish
-end
-
+# Set up ruby gems to use Homebrew's readline
 set -x RUBY_CONFIGURE_OPTS --with-readline-dir=(brew --prefix readline)
 
 # If `.private.sh` exsits, load it
@@ -51,9 +39,3 @@ if test -s "$HOME/.private.sh" ; source "$HOME/.private.sh" ; end
 # Enable rbenv
 set -gx RBENV_ROOT /usr/local/var/rbenv
 . (rbenv init -|psub)
-
-
-# set -x GOPATH /Users/ehyland/Code/go_crap
-# set -x GOBIN /Users/ehyland/Code/go_crap/bin
-
-# set -x PATH $PATH $GOBIN
