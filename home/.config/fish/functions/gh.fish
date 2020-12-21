439692b06f74a9da2e6c5d61b4a8fd6ba44c5f73
Emily Hyland <hello@duien.com>
2020-12-21 09:59:14 -0800

    Add gh alias

---
+++ home/.config/fish/functions/gh.fish
@@ -0,0 +1,10 @@
+function gh --description 'gh pr status'
+	if test -z "$argv"
+    command gh pr status
+  else if test "draft" = $argv
+    # IDEA get pivotal story from branch name
+    command gh pr --draft
+  else
+    command gh $argv
+  end
+end
