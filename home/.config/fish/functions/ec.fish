a5b67d384bc2e40c13af3f02e2e4f8ffbf8f742c
Emily Hyland <hello@duien.com>
2022-02-16 15:14:31 -0800

    Make ec (mostly) work for terminal

--- home/.config/fish/functions/ec.fish
+++ home/.config/fish/functions/ec.fish
@@ -1,5 +1,6 @@
 function ec --description "open emacs client, starting server if needed"
     argparse -i 'p/with-profile=' -- $argv
+    argparse -i 't/tty' -- $argv
     set -l socket_file
     if test -n "$_flag_p"
         set socket_file (emacsserver -p$_flag_p)
@@ -16,6 +17,10 @@ function ec --description "open emacs client, starting server if needed"
         end
         disown
     else
-        emacsclient -n $argv --socket-name $socket_file
+        set -l clientflag "-n"
+        if test -n "$_flag_tty"
+            set clientflag "-t"
+        end
+        emacsclient $clientflag $argv --socket-name $socket_file
     end
 end
