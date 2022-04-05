7de258fedf8f3d233d4f27c1a6c575ed45e39938
Emily Hyland <hello@duien.com>
2022-04-05 12:55:22 -0700

    Continuing work on config revision

--- home/.scratch-config/init.el
+++ home/.scratch-config/init.el
@@ -1,3 +1,24 @@
+;; Bootstrap straight
+(setq straight-use-package-by-default t)
+(setq straight-vc-git-default-clone-depth 1)
+(setq straight-recipes-gnu-elpa-use-mirror t)
+(setq straight-check-for-modifications nil)
+(setq use-package-always-defer t)
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(straight-use-package 'use-package)
+
+
 (org-babel-load-file "~/.scratch-config/revision.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
