d58deaa709d3ac2423248ec109caf09d5a84625c
Emily Hyland <hello@duien.com>
2022-05-13 23:38:26 -0700

    Cleanup, get initial font and theme setup working again

--- home/.scratch-config/init.el
+++ home/.scratch-config/init.el
@@ -21,16 +21,3 @@
 (straight-use-package 'org)
 
 (org-babel-load-file "~/.scratch-config/revision.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "c4ebf50856d3685944e5866c56d0f4073e2242fb61e02b32b3aba598abcd033c" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
