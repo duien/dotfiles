cf8ad61328d1a940c8c5ef8a4f1e0775e9b2b0b3
Emily Hyland <hello@duien.com>
2022-09-14 11:00:33 -0700

    Clean up profiles, make minimal more minimal

--- home/.minimal-config/init.el
+++ home/.minimal-config/init.el
@@ -1,11 +1,3 @@
-;; A couple basic modus settings
-(setq modus-themes-mixed-fonts nil)
-(setq modus-themes-italic-constructs t)
-
-;; And a couple basic org settings
-(setq org-fontify-todo-headline t)
-(setq org-fontify-done-headline t)
-
 ;; Try not to go crazy while testing
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
@@ -13,23 +5,7 @@
 
 ;; Set up font faces
 (set-face-attribute 'default nil
-		    :font "Rec Mono Duotone" :height 160)
-(set-face-attribute 'fixed-pitch nil
-		    :family (face-attribute 'default :family))
-
-;; Override faces for Org
-;; Do this in a function that can be added to the theme
-;; load hook to make testing less painful
-(defun eh/modus-org-faces ()
-  (set-face-attribute 'org-headline-todo nil
-		      :weight 'normal :inherit 'default)
-  (set-face-attribute 'org-headline-done nil
-		      :weight 'normal :slant 'italic)
-  )
-(add-hook 'modus-themes-after-load-theme-hook #'eh/modus-org-faces)
-(add-hook 'org-mode-hook #'eh/modus-org-faces) ;; hacky
+		    :font "Menlo" :height 160)
 
 ;; And finally, load modus
 (load-theme 'modus-operandi)
-
-
