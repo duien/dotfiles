57c67e7996707f36575ebaa0cfa041c5667c32e3
Emily Hyland <hello@duien.com>
2022-10-28 13:39:18 -0700

    Delete a whole lot of old stuff

--- home/.castlemacs/private.el
+++
@@ -1,7 +0,0 @@
-;; This is your private configuration file. It is loaded automatically, so feel free to add whatever you want.
-;; This file will not be affected by Castlemacs updates.
-
-(use-package moody
-  :init
-  (moody-replace-mode-line-buffer-identification)
-  )
--- home/.gvimrc
+++
@@ -1,7 +0,0 @@
-set guifont=Consolas:h16
-" set guifont=Anonymous\\ Pro:h16
-" set guifont=Anonymous\ Pro:h14
-set vb
-" set noantialias
-set colorcolumn=80  " mark 80 characters
-set guifont=M+\ 2m:h16
